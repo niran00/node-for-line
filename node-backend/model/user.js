0653ff61a7bc50f5b14181036dd60cf4edc72aa6
Niran <niran.millet@gmail.com>
2022-03-29 17:41:27 +0700

    end day push

--- node-backend/model/user.js
+++ node-backend/model/user.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const uniqueValidator = require('mongoose-unique-validator');
  
 let User = new Schema({
   userId: {
-    type: String
+    type: String, require: true, unique: true 
   },
   userName: {
     type: String
   },
   userPhoneNumber: {
-    type: String
+    type: String, require: true, unique: true  
   }
 }, {
   collection: 'users'
 })
+
+User.plugin(uniqueValidator);
  
 module.exports = mongoose.model('User', User)
