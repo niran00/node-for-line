1aef120fe5e6ea10b199355089d51cd42e605204
Niran <niran.millet@gmail.com>
2022-03-30 13:09:52 +0700

    token route

--- node-backend/routes/user.routes.js
+++ node-backend/routes/user.routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
-
 const userRoute = express.Router();
 let User = require('../model/user.js');
 
@@ -67,5 +66,32 @@ userRoute.route('/delete-user/:id').delete((req, res, next) => {
   })
 })
 
+//Login
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ userId: req.body.userId })
+  .then(user => {
+    if(!user){
+      return res.status(404).json({
+        message: 'auth failed'
+      });
+    }  
+    fetchedUser = user;
+    const token = jwt.sign(
+      { userId: fetchedUser.userId, unqUserId: fetchedUser._id },
+      'this_is_the_secret',
+      {expiresIn: '1h' }
+    );
+    res.status(200).json({
+      token: token
+    })
+  })
+  .catch(err =>{
+    return res.status(404).json({
+      message: 'auth failed, not successful'
+    });
+  });
+
+});
 
 module.exports = userRoute;
