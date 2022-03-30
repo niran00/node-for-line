19203a42356e6deda6c26ab5ecedae2cb6167c7a
Niran <niran.millet@gmail.com>
2022-03-30 16:18:22 +0700

    auth books all good

--- node-backend/middleware/check-auth.js
+++ node-backend/middleware/check-auth.js
@@ -4,6 +4,7 @@ module.exports = function authenticateToken(req, res, next){
     try{
         const token = req.headers.authorization.split(" ")[1];
         jwt.verify(token, 'this_is_the_secret');
+        console.log("all good");
         next();
     } catch(error){
         res.status(401).json({message: "login failed"})
