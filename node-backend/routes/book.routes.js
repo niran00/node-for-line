c47a2a32e2e9aa5bde7a324aca7ed4716fd824fd
Niran <niran.millet@gmail.com>
2022-03-30 15:01:50 +0700

    del auth

--- node-backend/routes/book.routes.js
+++ node-backend/routes/book.routes.js
@@ -54,7 +54,7 @@ bookRoute.route('/update-book/:id' , checkAuth).put((req, res, next) => {
 })
 
 // Delete Book
-bookRoute.route('/delete-book/:id').delete((req, res, next) => {
+bookRoute.route('/delete-book/:id',  checkAuth).delete((req, res, next) => {
   checkAuth,
   Book.findByIdAndRemove(req.params.id, (error, data) => {
     if (error) {
