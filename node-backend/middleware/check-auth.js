c4f792e1aeebb3c07034eba9a1055f060a0a8e3c
Niran <niran.millet@gmail.com>
2022-03-30 16:04:31 +0700

    auth books method

--- node-backend/middleware/check-auth.js
+++ node-backend/middleware/check-auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
+module.exports = function authenticateToken(req, res, next){
     try{
         const token = req.headers.authorization.split(" ")[1];
         jwt.verify(token, 'this_is_the_secret');
