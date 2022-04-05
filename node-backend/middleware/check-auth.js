1883d6ffb8786ecdf98f26d0e972d98ee1f8db4a
Niran <niran.millet@gmail.com>
2022-04-05 12:28:16 +0700

    decoded Token

--- node-backend/middleware/check-auth.js
+++ node-backend/middleware/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(token, 'this_is_the_secret');
+        const decodedToken  = jwt.verify(token, 'this_is_the_secret');
         req.userData = {userId: decodedToken.userId }
         next();
     } catch(error){
