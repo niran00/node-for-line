7601305fd6e2a1a706ad2a443f2c2d3edb013ebd
Niran <niran.millet@gmail.com>
2022-04-18 17:30:35 +0700

    final day push

--- node-backend/model/user.js
+++ node-backend/model/user.js
@@ -15,7 +15,7 @@ let User = new Schema({
   userPhoneNumber: {
     type: String, require: true, unique: true  
   },
-  userOTPToken:{
+  userOtpToken:{
     type: String, require: true, unique: true  
   }
 }, {
