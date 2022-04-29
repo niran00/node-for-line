7bd6907143def8d64e72a2f3faae8087680bcdf0
Niran <niran.millet@gmail.com>
2022-04-29 18:26:25 +0700

    multer images added

--- node-backend/model/book.js
+++ node-backend/model/book.js
@@ -1,20 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
- 
-let Book = new Schema({
-  name: {
-    type: String
-  },
-  price: {
-    type: String
-  },
-  description: {
-    type: String
-  }, 
-  creator: { type : mongoose.Schema.Types.String, ref: "User", required: true}
 
-}, {
-  collection: 'books'
-})
- 
-module.exports = mongoose.model('Book', Book)
+let Book = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    price: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    creator: {
+      type: mongoose.Schema.Types.String,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    collection: "books",
+  }
+);
+
+module.exports = mongoose.model("Book", Book);
--- node-backend/routes/book.routes.js
+++ node-backend/routes/book.routes.js
@@ -1,77 +1,113 @@
-const express = require('express');
+const express = require("express");
+const multer = require("multer");
 const app = express();
-let checkAuth = require('../middleware/check-auth.js');
+let checkAuth = require("../middleware/check-auth.js");
 const bookRoute = express.Router();
-let Book = require('../model/book.js');
+let Book = require("../model/book.js");
 
-// Add Book
-bookRoute.route('/add-book').post( checkAuth, (req, res, next) => {
-  const bookdata = new Book({
-    name: req.body.name ,
-    price: req.body.price,
-    description: req.body.description,
-    creator : req.userData.userId,
-  });
-  Book.create(bookdata, (error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      console.log(req.userData);
-      res.json(data)
+const MINE_TYPE = {
+  "image/png": "png",
+  "image/jpg": "jpg",
+  "image/jpeg": "jpeg",
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MINE_TYPE[file.mimetype];
+    let error = new Error("Invalid mine type");
+    if (isValid) {
+      null;
     }
-  })
+    cb(null, "assets/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLocaleLowerCase().split(" ").join("-");
+    const ext = MINE_TYPE[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + ext);
+  },
 });
 
+// Add Book
+bookRoute
+  .route("/add-book")
+  .post(
+    checkAuth,
+    multer({ storage: storage }).single("image"),
+    (req, res, next) => {
+      const imgurl = req.protocol + "://" + req.get("host");
+      const bookdata = new Book({
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        creator: req.userData.userId,
+        imagePath: imgurl + "/images/" + req.file.filename,
+      });
+      Book.create(bookdata, (error, data) => {
+        if (error) {
+          return next(error);
+        } else {
+          console.log(req.userData);
+          res.json(data);
+        }
+      });
+    }
+  );
+
 // Get all Book
-bookRoute.route('/').get( checkAuth, (req, res) => {
+bookRoute.route("/").get(checkAuth, (req, res) => {
   Book.find((error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      res.json(data)
-    }
-  })
-})
-
-// Get Book
-bookRoute.route('/read-book/:id').get( (req, res) => {
-  Book.findById(req.params.id, (error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      res.json(data)
-    }
-  })
-})
-
-
-// Update Book
-bookRoute.route('/update-book/:id').put( checkAuth, (req, res, next) => {
- 
-  Book.updateOne({_id: req.params.id, creator: req.userData.userId},  {
-    $set: req.body,
-  },  (error, data) => {
-    if (data.modifiedCount > 0) {
-      res.status(200).json({message: "updated successfully"});
-      console.log('Book updated successfully!');
-    } else {
-      res.status(401).json({message: "Not Authorized"});
-      console.log('User Not Authorized');
-    }
-  })
-})
-
-// Delete Book
-bookRoute.route('/delete-book/:id').delete(checkAuth, ( req, res, next) => {
-  Book.findOneAndRemove({_id:req.params.id, creator: req.userData.userId }, (error, data) => {
     if (error) {
       return next(error);
     } else {
-      res.status(200).json({
-        msg: data
-      })
+      res.json(data);
     }
-  })
-})
+  });
+});
+
+// Get Book
+bookRoute.route("/read-book/:id").get((req, res) => {
+  Book.findById(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+// Update Book
+bookRoute.route("/update-book/:id").put(checkAuth, (req, res, next) => {
+  Book.updateOne(
+    { _id: req.params.id, creator: req.userData.userId },
+    {
+      $set: req.body,
+    },
+    (error, data) => {
+      if (data.modifiedCount > 0) {
+        res.status(200).json({ message: "updated successfully" });
+        console.log("Book updated successfully!");
+      } else {
+        res.status(401).json({ message: "Not Authorized" });
+        console.log("User Not Authorized");
+      }
+    }
+  );
+});
+
+// Delete Book
+bookRoute.route("/delete-book/:id").delete(checkAuth, (req, res, next) => {
+  Book.findOneAndRemove(
+    { _id: req.params.id, creator: req.userData.userId },
+    (error, data) => {
+      if (error) {
+        return next(error);
+      } else {
+        res.status(200).json({
+          msg: data,
+        });
+      }
+    }
+  );
+});
 
 module.exports = bookRoute;
